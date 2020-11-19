ecf7f8209d17d94848b2e2955a4a4f5a484d7ce6
Piyush <>
2020-11-19 22:39:23 +0530

    latest

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -101,7 +101,7 @@ ASGI_APPLICATION = 'mysite.routing.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
